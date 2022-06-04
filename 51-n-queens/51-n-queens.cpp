d8f2379e9d5160f9ef7849220d20ea25f85888c0
Utkarsh tripathi <76621101+Utkarsh41@users.noreply.github.com>
2022-06-05 02:15:26 +0530

    Time: 4 ms (88.75%), Space: 7.3 MB (67.78%) - LeetHub

---
+++ 51-n-queens/51-n-queens.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector< vector<string> >ans;
+  
+    bool visCols[9],mainDiag[20],secDiag[20];    
+    void doWork(int row,int n,vector<string>&board)
+    {
+        if(row==n)
+        {
+            ans.push_back(board);
+            return;
+        }
+        for(int col=0;col<n;col++)
+        {
+            if(!visCols[col] && !mainDiag[n+row-col] && !secDiag[row+col])
+            {
+                board[row][col]='Q';
+                visCols[col] = mainDiag[n+row-col] = secDiag[row+col] = true;
+                doWork(row+1,n,board);
+                visCols[col] = mainDiag[n+row-col] = secDiag[row+col] = false;
+                board[row][col]='.';
+            }
+        }
+    }
+    
+    
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string>board(n,string(n,'.'));
+        doWork(0,n,board);
+        return ans;
+    }
+};
