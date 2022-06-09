573e422c5d95ba547a1403898ca985c689b4e6d8
Utkarsh tripathi <76621101+Utkarsh41@users.noreply.github.com>
2022-06-09 23:09:52 +0530

    Time: 0 ms (100.00%), Space: 6 MB (26.20%) - LeetHub

---
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:Solution() { ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr); }
+
+    int getSum(int a, int b) {
+        
+        return a+b;
+        
+    }
+};
