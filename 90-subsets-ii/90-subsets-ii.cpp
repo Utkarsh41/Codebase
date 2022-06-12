1778097cb4f609c251a6c55d4df3fcde8d8e1971
Utkarsh tripathi <76621101+Utkarsh41@users.noreply.github.com>
2022-06-12 16:37:10 +0530

    Time: 3 ms (81.13%), Space: 7.6 MB (73.64%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    
+    
+      void ut(int idx,vector<int>& nums,vector<int>& ds, vector<vector<int>>&ans){
+            ans.push_back(ds);
+         
+          for(int i=idx;i<nums.size();i++){
+              if(i!=idx && nums[i]==nums[i-1]){continue;}
+                ds.push_back(nums[i]);
+                ut(i+1,nums,ds,ans);
+                ds.pop_back();            
+          }      
+        
+    }
+    
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>ds;
+        sort(nums.begin(),nums.end());
+        ut(0,nums,ds,ans);
+        return ans;
+    }
+};
