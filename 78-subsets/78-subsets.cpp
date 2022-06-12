a2368b0cc217a2a2b4c3e13c9a4c402181c798e9
Utkarsh tripathi <76621101+Utkarsh41@users.noreply.github.com>
2022-06-12 16:29:17 +0530

    Time: 0 ms (100.00%), Space: 7 MB (86.59%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    
+    
+    void ut(int i,vector<int>& nums,vector<int>& ds, vector<vector<int>>&ans){
+        
+        if(i==nums.size()){
+            ans.push_back(ds);
+            return;
+        }
+        ds.push_back(nums[i]);
+        ut(i+1,nums,ds,ans);
+        ds.pop_back();
+        ut(i+1,nums,ds,ans);
+
+        
+        
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+         vector<vector<int>>ans;
+        vector<int>ds;
+        
+        ut(0,nums,ds,ans);
+        return ans;
+    }
+};
