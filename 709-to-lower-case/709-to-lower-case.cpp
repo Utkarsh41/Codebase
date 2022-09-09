d903f877cf9cca43111ef62399110554798ff27f
Utkarsh tripathi <76621101+Utkarsh41@users.noreply.github.com>
2022-09-09 18:22:51 +0530

    Time: 5 ms (18.53%), Space: 6.2 MB (53.99%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.cpp
+++ 709-to-lower-case/709-to-lower-case.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     string toLowerCase(string s) {
         for(int i=0;i<s.length();i++){
-            if(isupper(s[i])){
-                s[i]=tolower(s[i]);
+//             
+            if((s[i]>='A' && s[i]<='Z')){
+                // s[i]=tolower(s[i]);
+                s[i]=s[i]+'a'-'A';
             }
         }
         return s;
