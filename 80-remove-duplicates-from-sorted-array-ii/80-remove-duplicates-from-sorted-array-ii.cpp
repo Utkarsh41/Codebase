3f009f4710beed84730ae3d5133fcdb754a7c454
Utkarsh tripathi <76621101+Utkarsh41@users.noreply.github.com>
2022-09-14 10:39:17 +0530

    Time: 7 ms (76.21%), Space: 11.7 MB (6.37%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    Solution(){ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);}
     int removeDuplicates(vector<int>& nums) {
         
         int n=nums.size();
