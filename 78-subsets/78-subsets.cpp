572b8e58fe8c878dee117819580dbd3c538f1ab5
Utkarsh tripathi <76621101+Utkarsh41@users.noreply.github.com>
2022-09-20 22:10:57 +0530

    Time: 3 ms (75.50%), Space: 7.3 MB (58.01%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
-    
+    Solution(){
+        ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(0);
+    }
     
     void ut(int i,vector<int>& nums,vector<int>& ds, vector<vector<int>>&ans){
         
@@ -18,7 +20,7 @@ public:
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         
-         vector<vector<int>>ans;
+        vector<vector<int>>ans;
         vector<int>ds;
         
         ut(0,nums,ds,ans);
