cef43b503c1f242b0475f8e4f9296c8d42548a18
Utkarsh tripathi <76621101+Utkarsh41@users.noreply.github.com>
2022-10-11 19:10:48 +0530

    Time: 12 ms (97.37%), Space: 16.7 MB (41.85%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,6 +1,6 @@
-class Solution {
+class Solution1 {
 public:
-    Solution(){ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);}
+    Solution1(){ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);}
     int jump(vector<int>& nums) {
         
         int n=nums.size();
@@ -19,6 +19,30 @@ public:
             }
         }
         return ut[0];
+        
+    }
+};
+class Solution {
+public:
+    Solution(){ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);}
+    int jump(vector<int>& nums) {
+        
+        int n=nums.size();
+        // if(n==0)return 0;
+       
+        int end=nums[0];
+        int me=nums[0];
+        int jump=1;
+        if(n==1)return 0;
+        
+        for(int i=1;i<n-1;i++){
+            me=max(me,i+nums[i]);
+            if(i==end){
+                jump++;
+                end=me;
+            }
+        }
+        return jump;
         
     }
 };
