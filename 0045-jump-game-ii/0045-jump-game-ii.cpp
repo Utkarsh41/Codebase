648e15eb96239549fce52b770612fc1f7c886bd8
Utkarsh tripathi <76621101+Utkarsh41@users.noreply.github.com>
2022-10-11 18:45:56 +0530

    Time: 429 ms (34.91%), Space: 17.1 MB (40.78%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        
+        int n=nums.size();
+        if(n==0)return 0;
+        vector<int>ut(n,INT_MAX);
+        ut[n-1]=0;
+        
+        for(int i=n-2;i>=0;i--){
+            int res=INT_MAX;
+            for(int j=i+1;j<=min(n-1,i+nums[i]);j++){
+                res=min(res,ut[j]);
+            }
+            if(res!=INT_MAX){
+                ut[i]=res+1;
+            }
+        }
+        return ut[0];
+        
+    }
+};
