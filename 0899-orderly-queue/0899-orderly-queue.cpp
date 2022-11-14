322a29c3515a06bd9f9ab8ae8f4e3010940e6597
Utkarsh tripathi <76621101+Utkarsh41@users.noreply.github.com>
2022-11-14 09:47:21 +0530

    Time: 10 ms (31.03%), Space: 8.2 MB (62.58%) - LeetHub

---
+++ 0899-orderly-queue/0899-orderly-queue.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string orderlyQueue(string s, int k) {
+        
+       if (k == 1) {
+            string ans = s;
+            // Append |s| to itself and look for the smallest string
+            // that has the same lenght as |s|.
+            s += s;
+            for (int i = 1; i < size(ans); ++i)
+                ans = min(ans, s.substr(i, size(ans)));
+
+            return ans;
+        }
+        
+        // With k > 1 we can reorder |s| how we want, so sorting it
+        // will yield the lexicographically smallest one.
+        sort(begin(s), end(s));
+        return s;
+        
+    }
+};
