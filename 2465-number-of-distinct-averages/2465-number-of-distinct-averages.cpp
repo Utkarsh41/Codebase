5465ff386022b73d27e458326fc15ccc7827f85f
Utkarsh tripathi <76621101+Utkarsh41@users.noreply.github.com>
2022-11-16 12:31:48 +0530

    Time: 4 ms (39.87%), Space: 7.3 MB (29.40%) - LeetHub

--- 2465-number-of-distinct-averages/2465-number-of-distinct-averages.cpp
+++ 2465-number-of-distinct-averages/2465-number-of-distinct-averages.cpp
@@ -9,15 +9,15 @@ public:
         float avg=0.0f;
         set<float>st;
         
-        for(auto c:arr)cout<<c<<" ";
-        cout<<endl;
+        // for(auto c:arr)cout<<c<<" ";
+        // cout<<endl;
         
         int l=0,h=nums.size()-1;
         
         while(l<=h){
             
             avg=(float)(arr[l++]+arr[h--])/2;
-            cout<<avg<<endl;
+            // cout<<avg<<endl;
             st.insert(avg);
             
         }
