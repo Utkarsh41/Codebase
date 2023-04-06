9e0f8df1041e9d9483e0cace1ba886311a9df7f2
Utkarsh tripathi <76621101+Utkarsh41@users.noreply.github.com>
2023-04-06 18:57:08 +0530

    Time: 21 ms (86.53%), Space: 13.9 MB (64.36%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
       void dfs(int root, vector<int>& vis, vector<int> adj[]){
         if (vis[root])return;
@@ -29,5 +29,41 @@ public:
         }
         return cnt;
         
+    }
+};
+
+class Solution {
+public:
+
+    vector<int>v;
+	
+    int find_set(int x){
+        if(!v[x])
+            return x;
+        return v[x]=find_set(v[x]);
+    }
+	
+    void union_(int a,int b){
+        int s_a=find_set(a);
+        int s_b=find_set(b);
+        if(s_a==s_b) return;
+        else v[s_a]=s_b;
+    }
+	
+    int findCircleNum(vector<vector<int>>& M) {
+        ios_base::sync_with_stdio(false);cin.tie(NULL);
+        int n=M.size();
+        vector<int> parent(n);
+        v= vector<int>(n,0);
+        for(int i=0;i<n;i++)
+            for(int j=0;j<n;j++){
+            if(M[i][j])
+                union_(i,j);
+        }
+			int c=0;
+			for(auto i:v)
+				if(!i) c++;
+        
+        return c;
     }
 };
