34eff285ed283338fb2403e7eb43bfee72c2f4c4
Utkarsh tripathi <76621101+Utkarsh41@users.noreply.github.com>
2023-04-17 18:02:57 +0530

    Time: 273 ms (5.00%), Space: 38.7 MB (57.38%) - LeetHub

---
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int mostFrequentEven(vector<int>& nums) {
+        
+        map<int,int>mp;
+        
+        int n=nums.size();
+        
+        for(int i=0;i<n;i++){
+            
+            if(nums[i]%2==0){
+                mp[nums[i]]++;
+            }
+            
+        }
+        int mostFreq=-1,curr=-1;
+        for(auto nm:mp){
+            cout<<nm.first<<"-"<<nm.second<<endl;
+           
+            if(curr<nm.second){
+                curr=nm.second;
+                mostFreq=nm.first;
+            }
+            else if(curr==nm.second){
+                mostFreq=min(mostFreq,nm.first);
+            }
+            
+        }
+        
+        return mostFreq;
+    }
+};
